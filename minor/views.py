d2968850c04351b577e95788b91096c426db0654
Aiym <bakaevaaiyma7@gmail.com>
2021-12-28 01:30:21 +0600

    character

--- minor/views.py
+++ minor/views.py
@@ -11,7 +11,25 @@ class MainPageView(ListView):
     model = Category
     template_name = 'minor/index.html'
     context_object_name = 'categories'
-    paginate_by = 2
+    paginate_by = 3
+
+    # def get_template_names(self):
+    #     template_name = super(MainPageView, self).get_template_names()
+    #     search = self.request.GET.get('search')
+    #     if search:
+    #         template_name = 'search.html'
+    #     return template_name
+    #
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     search = self.request.GET.get('search')
+    #     if search:
+    #         context['categories'] = Category.objects.filter(Q(title__icontains=search) | Q(description__icontains=search))
+    #
+    #     else:
+    #         context['categories'] = Category.objects.all()
+    #     return context
+    #
 
 
 def contacts(request):
