50f261d4e16090b35b623f3ab39f196913a30851
Bermet <bermetibr@gmail.com>
2021-12-29 15:29:47 +0600

    comments and blog finished

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-from account.forms import User
 
 STATUS = ((0, "Draft"), (1, "Publish"))
+
+User = get_user_model()
 
 
 class Blog(models.Model):
@@ -13,6 +15,7 @@ class Blog(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    image = models.ImageField(blank=True, null=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -28,13 +31,14 @@ class Blog(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
-    email = models.EmailField()
+    email = models.ManyToManyField(User)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
+
 
     def __str__(self):
         return "Comment {} by {}".format(self.body, self.name)
